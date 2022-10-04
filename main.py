fa6501d47a6b66b26cff4df89da74f040dbad0ca
RegishShrestha <regishshrestha@gmail.com>
2022-10-04 09:47:19 +0545

    navbar and footer added using jinger

--- main.py
+++ main.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html",)
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/post')
+def post():
+    return render_template("post.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 
 
