408e56738ed829cce66cc79f0fadd0a9d08617df
RegishShrestha <regishshrestha@gmail.com>
2022-10-05 20:07:56 +0545

    individual posts added

--- main.py
+++ main.py
@@ -3,20 +3,28 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
     
-response=requests.get("https://api.npoint.io/18aae9aafedc1b39590e")
-data=response.json()
 
 @app.route('/')
 def home():
+    response=requests.get("https://api.npoint.io/18aae9aafedc1b39590e")
+    data=response.json()
     return render_template("index.html",all_data=data)
 
 @app.route('/about')
 def about():
     return render_template("about.html")
 
-@app.route('/post')
-def post():
-    return render_template("post.html")
+@app.route('/post/<id>')
+def post(id):
+    print(id)
+    response=requests.get("https://api.npoint.io/18aae9aafedc1b39590e")
+    datas=response.json()
+    for data in datas:
+        if int(data['id'])==int(id):
+            data_got=data        
+    return render_template("post.html",data=data_got)
+    # return render_template("post.html")
+    
 
 @app.route('/contact')
 def contact():
