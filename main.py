b27ebc10479b35eb814abbe97914d50d6a9398d1
RegishShrestha <regishshrestha@gmail.com>
2022-10-09 18:47:44 +0545

    contact completed

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import smtplib
 import requests
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+
+my_email="regishshrestha@gmail.com"
+password="dmreixqeuionlbxx"
 
 app = Flask(__name__)
     
@@ -26,11 +30,34 @@ def post(id):
     # return render_template("post.html")
     
 
-@app.route('/contact')
+@app.route('/contact',methods=['POST','GET'])
 def contact():
-    return render_template("contact.html")
+    if request.method == 'GET':
+        return render_template("contact.html",msg_sent=False)
+    else:
+        name=request.form['name']  
+        email=request.form['email']
+        phone_number=request.form['phone_number']
+        message=request.form['message']
+        print(name,email,phone_number,message)
+        connection=smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(user=my_email,password=password)
+        connection.sendmail(from_addr=my_email,to_addrs="regishshrestha@gmail.com",             msg=f"Subject:New Message \n\n Name: {name} \n email: {email} \n Phone Number: {phone_number} \n Message: {message}")
+        connection.close()
+        return render_template("contact.html",msg_sent=True)
+        
 
 
+# @app.route('/form-entry',methods=['POST',"GET"])
+# def receive_data():
+#         name=request.form['name']
+#         email=request.form['email']
+#         phone_number=request.form['phone_number']
+#         message=request.form['message']
+#         print(name,email,phone_number,message)
+#         if request.method == 'POST':
+#             return "Sucessfully send the message"
 
 if __name__ == "__main__":
     app.run(debug=True)
